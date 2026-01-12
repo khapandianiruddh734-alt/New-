d6e5004dceccfb9fc5b1eb687ff4da480902ecee
Anirudhdh <khapandianiruddh734@gmail.com>
2026-01-12 14:55:05 +0530

    admin Page

--- making-of-the-new-app (3)/index.tsx
+++ making-of-the-new-app (3)/index.tsx
@@ -1,16 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Admin from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './Admin';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Admin />
   </React.StrictMode>
 );
